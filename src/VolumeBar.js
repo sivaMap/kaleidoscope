9fc4f55a939f6962bf6d549f0b5ed767d788da04
sivaMap <sivalingam.s@map-india.org>
2024-10-01 13:07:29 +0530

    is show running update functinality

--- src/VolumeBar.js
+++ src/VolumeBar.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VolumeSlider from './components/VolumeSlider'
 import VolumeSlider2 from './components/VolumeSlider2'
 
-const VolumeBar = () => {
+const VolumeBar = ({ isShowRunning }) => {
+  // const [play, setPlay] = useState(true);
+  const [play, setPlay] = useState(isShowRunning);  
+  useEffect(() => setPlay(true), [isShowRunning]);
+  
   return (
     <div className='bg-black bg-opacity-30 w-full h-[50px] p-1 text-center text-2xl flex-shrink-0 flex-none flex justify-between'>
       {/* class="flex flex-col justify-center w-[180px] h-[80px] -pl-5" */}
       {/* <VolumeSlider/> */}
 
-      <div className='flex flex-col justify-center'>
-        <VolumeSlider2 />
+      <div className={`flex flex-col justify-center ${!isShowRunning ? "opacity-30" : ""}`}>
+        <VolumeSlider2 isShowRunning={isShowRunning} />
       </div>
 
-      <div className='flex flex-col justify-center'>
+      <div className={`flex flex-col justify-center ${!isShowRunning ? "opacity-30" : ""}`}>
         <img
-          src={"/images/volumeBar/play.png"} alt="Low Volume"
-          className='h-[30px]' />
+          // src={!isShowRunning || play ? "/images/volumeBar/play.png" : "/images/volumeBar/pause.png"}
+          // src={!isShowRunning || play ? "/images/volumeBar/play.png" : "/images/volumeBar/pause.png"}
+          src={play ? "/images/volumeBar/play.png" : "/images/volumeBar/pause.png"}
+          alt={play ? "Play" : "Pause"}
+          className='h-[30px]'
+          onClick={() => isShowRunning ? setPlay(prev => !prev) : setPlay(true)}
+        />
       </div>
 
       <div className='flex flex-col justify-center'>
