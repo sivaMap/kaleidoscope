da14c6595db55b8b8f2aad3b60380ecc74b81c7a
sivaMap <sivalingam.s@map-india.org>
2024-10-01 13:06:11 +0530

    is show running setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Kaleidoscope from "./Kaleidoscope";
 import TaskBar from "./TaskBar";
 import VolumeBar from "./VolumeBar";
+import { constants } from "./constants";
 import "./css/font.css"
 import "./css/index.css"
 
 function App() {
-  return (    
+  const [isShowRunning, setIsshowRunning] = useState(constants.isShowRunning);
+
+  return (
     <div className="bg-[#45275E] h-screen flex flex-col "
     >
       <TaskBar />
-      <Kaleidoscope />
-      <VolumeBar />
+      <Kaleidoscope setIsshowRunning={setIsshowRunning} />
+      <VolumeBar isShowRunning={isShowRunning} />
     </div>
   );
 }
--- src/Kaleidoscope.js
+++ src/Kaleidoscope.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import CuratedShows from './components/CuratedShows'
 import ArtWork from './components/ArtWork'
+import { constants } from './constants'
 
-const Kaleidoscope = () => {
+const Kaleidoscope = (props) => {
+  const { setIsshowRunning } = props;
 
   return (
     <div className='py-8 px-8   flex-grow overflow-y-auto'>
@@ -15,7 +17,10 @@ const Kaleidoscope = () => {
             Choose any one Curated Experience <strong className='text-white '>OR</strong> any 5 Artworks to start the experience
           </p>
           <p className='rounded-2xl border-white border-2 px-4 py-1 text-white'
-            style={{ fontFamily: 'Geometria' }}>
+            style={{ fontFamily: 'Geometria' }}
+            onClick={() => {
+              setIsshowRunning(prev => !prev)
+            }}>
             Next
           </p>
         </div>
