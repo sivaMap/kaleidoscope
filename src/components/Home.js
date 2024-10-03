c0da99f43074b85b4a4f4ee31b6fbcfd7907e245
sivaMap <sivalingam.s@map-india.org>
2024-10-03 17:39:17 +0530

    Home

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='flex flex-col gap-9 my-14'>
+            <div className='flex justify-center'>
+                <span
+                    style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
+                    className='border-white border-opacity-30 border-2 rounded-full px-12 py-6'>
+                    Experience a Curated Show
+                </span>
+            </div>
+
+            <div className='flex justify-center  items-center gap-5'>
+                <hr className="w-2/12 border-t border-gray-300 mx-2" />
+                <p >OR</p>
+                <hr className="w-2/12 border-t border-gray-300 mx-2" />
+            </div>
+
+            <div className='flex justify-center'>
+                <span
+                    style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
+                    className='border-white border-opacity-30 border-2 rounded-full px-16 py-6'>
+                    Create your own Show
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Home
