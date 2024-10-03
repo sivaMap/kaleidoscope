3a99e2049c2cfb17fefff3e050953cf3762d1f2a
sivaMap <sivalingam.s@map-india.org>
2024-10-03 18:46:48 +0530

    loadState, navigateHomeScreen function

--- src/context/kaleidoscopeCrudContext.js
+++ src/context/kaleidoscopeCrudContext.js
@@ -1,18 +1,28 @@
 import React, { createContext, useContext, useState } from 'react'
+import { constants } from '../constants';
 
 const kaleidoscopesCrudContext = createContext();
 
-export const KaleidoscopeCrudContext = ({children}) => {
+export const KaleidoscopeCrudContext = ({ children }) => {
     //used in volumeBar
+    const [loadName, setLoadName] = useState(constants.loadScreen.default);
     const [isShowRunning, setisShowRunning] = useState(false);
 
     const toggleShowRun = () => {
         setisShowRunning(prev => !prev);
     }
 
+    // state is update for Home Page Navigation
+    const navigateHomeScreen = () => {
+        setLoadName(constants.loadScreen.default);
+    }
+
     const value = {
+        loadName,
+        setLoadName,
         isShowRunning,
-        toggleShowRun
+        toggleShowRun,
+        navigateHomeScreen
     };
 
     return (
@@ -25,4 +35,4 @@ export const KaleidoscopeCrudContext = ({children}) => {
 
 export function useKaleidoCrud() {
     return useContext(kaleidoscopesCrudContext);
-  }
+}
