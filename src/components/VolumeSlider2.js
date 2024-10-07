9494e8232410278b4ba61eb619e7a82cca578148
sivaMap <sivalingam.s@map-india.org>
2024-10-07 17:14:51 +0530

    convert png images to svg

--- src/components/VolumeSlider2.js
+++ src/components/VolumeSlider2.js
@@ -24,21 +24,20 @@ const VolumeSlider2 = ({ isShowRunning }) => {
 
     return (
         !isSliderEnabled || !isShowRunning ?
-            <div className='w-44 ml-4'>
-                <img
-                    src={"/images/volumeBar/slider/lowVolume.png"}
-                    alt="Low Volume"
-                    className='h-[30px]'
-                    onClick={() => isShowRunning && setSliderEnable(prev => !prev)}
-                />
+            <div className='w-40 ml-2 pl-2 pr-0 py-1 rounded-full'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 38" fill="none"
+                    onClick={() => isShowRunning && setSliderEnable(prev => !prev)}>
+                    <path d="M0 25.9999V11.9999H9.33333L21 0.333252V37.6666L9.33333 25.9999H0ZM25.6667 28.3333V9.54992C27.4167 10.3666 28.8264 11.6305 29.8958 13.3416C30.9653 15.0527 31.5 16.9388 31.5 18.9999C31.5 21.061 30.9653 22.9277 29.8958 24.5999C28.8264 26.2721 27.4167 27.5166 25.6667 28.3333ZM16.3333 11.6499L11.3167 16.6666H4.66667V21.3333H11.3167L16.3333 26.3499V11.6499Z" fill="#BABABA" />
+                </svg>
             </div>
             :
             <div
-                className="volume-slider w-44 ml-4 pl-4 pr-0 py-1 rounded-full bg-black bg-opacity-30">
-                <img src={volume < volumeChanger ? lowVolume : highVolume} alt="High"
-                    className='h-[30px]'
-                    onClick={() => isShowRunning && setSliderEnable(prev => !prev)}
-                />
+                className="volume-slider w-40  pl-2 ml-2 pr-0 py-1 rounded-full bg-black bg-opacity-30">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="36" viewBox="0 0 32 38" fill="none"
+                    onClick={() => isShowRunning && setSliderEnable(prev => !prev)}>
+                    <path d="M0 25.9999V11.9999H9.33333L21 0.333252V37.6666L9.33333 25.9999H0ZM25.6667 28.3333V9.54992C27.4167 10.3666 28.8264 11.6305 29.8958 13.3416C30.9653 15.0527 31.5 16.9388 31.5 18.9999C31.5 21.061 30.9653 22.9277 29.8958 24.5999C28.8264 26.2721 27.4167 27.5166 25.6667 28.3333ZM16.3333 11.6499L11.3167 16.6666H4.66667V21.3333H11.3167L16.3333 26.3499V11.6499Z" fill="#BABABA" />
+                </svg>
+
 
                 <input type="range" className="slider"
                     min={0} max={200}
