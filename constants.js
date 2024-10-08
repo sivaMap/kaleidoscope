4fb6fb2d490546a318c83aad15b44ac05c3f4d92
sivaMap <sivalingam.s@map-india.org>
2024-10-08 18:23:05 +0530

    application constants

---
+++ constants.js
@@ -0,0 +1,6 @@
+exports.constants = {
+    loadScreen: { curate: "CuratedShow", art: "ArtShow", default: "Home" },
+    loadCurate: { show: "CuratedShows", play: "CuratedPlay" },
+    loadArt: { show: "ArtShows", play: "ArtPlay" },
+    backendUrl: "http://localhost:5001"
+}
