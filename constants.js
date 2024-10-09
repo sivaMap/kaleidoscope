ed6e76dbbb64d59652d0cd23c17bd576608a0324
sivaMap <sivalingam.s@map-india.org>
2024-10-09 18:23:48 +0530

    dynamic ip

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 exports.constants = {
     loadScreen: { curate: "CuratedShow", art: "ArtShow", default: "Home" },
     loadCurate: { show: "CuratedShows", play: "CuratedPlay" },
-    loadArt: { show: "ArtShows", play: "ArtPlay" },
-    backendUrl: "http://localhost:5001"
+    loadArt: { show: "ArtShows", play: "ArtPlay" },    
+    backendUrl: "http://192.168.68.128:5001"
 }
