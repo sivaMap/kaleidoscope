91278f5a1a1d33889098490357a033099dd3e70a
sivaMap <sivalingam.s@map-india.org>
2024-10-09 18:23:39 +0530

    rm fonts and implement {kaleidoscope and ipconfig}

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
-import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import TaskBar from './components/TaskBar';
 import VolumeBar from './components/VolumeBar';
 import { KaleidoscopeCrudContext } from './context/kaleidoscopeCrudContext';
+import Kaleidoscope from './components/Kaleidoscope';
+import IpConfig from './components/subcomponents/IpConfig';
+import React  from 'react';
 
 export default function App() {
-  const [fontsLoaded, error] = useFonts({
-    "Geometria-Bold": require("./assets/fonts/geometria/Geometria-Bold.ttf"),
-    "Geometria-BoldItalic": require("./assets/fonts/geometria/Geometria-BoldItalic.ttf"),
-    "Geometria-ExtraBold": require("./assets/fonts/geometria/Geometria-ExtraBold.ttf"),
-    "Geometria-ExtraBoldItalic": require("./assets/fonts/geometria/Geometria-ExtraBoldItalic.ttf"),
-    "Geometria-ExtraLight": require("./assets/fonts/geometria/Geometria-ExtraLight.ttf"),
-    "Geometria-ExtraLightItalic": require("./assets/fonts/geometria/Geometria-ExtraLightItalic.ttf"),
-    "Geometria-Heavy": require("./assets/fonts/geometria/Geometria-Heavy.ttf"),
-    "Geometria-HeavyItalic": require("./assets/fonts/geometria/Geometria-HeavyItalic.ttf"),
-    "Geometria-Italic": require("./assets/fonts/geometria/Geometria-Italic.ttf"),
-    "Geometria-Light": require("./assets/fonts/geometria/Geometria-Light.ttf"),
-    "Geometria-LightItalic": require("./assets/fonts/geometria/Geometria-LightItalic.ttf"),
-    "Geometria-MediumItalic": require("./assets/fonts/geometria/Geometria-MediumItalic.ttf"),
-    "Geometria-Thin": require("./assets/fonts/geometria/Geometria-Thin.ttf"),
-    "Geometria-ThinItalic": require("./assets/fonts/geometria/Geometria-ThinItalic.ttf"),
-    "Geometria": require("./assets/fonts/geometria/Geometria.ttf"),
-  });
 
   return (
     // <SafeAreaView className="flex-1 bg-black bg-opacity-80 relative flex-col gap-10">
@@ -30,10 +15,11 @@ export default function App() {
       style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}
       className="flex-1 relative flex-col justify-between h-screen"
     >
-      <KaleidoscopeCrudContext>        
+      <KaleidoscopeCrudContext>
         <TaskBar />
-        {/* <Kaleidoscope /> */}
-        <VolumeBar />        
+        <Kaleidoscope />
+        <VolumeBar />
+        <IpConfig />
       </KaleidoscopeCrudContext>
       <StatusBar style="auto" hidden={true} />
     </SafeAreaView>
