34b1e1a75da94c8891daaa9e5f0a920c4ab9cad7
sivaMap <sivalingam.s@map-india.org>
2024-10-10 18:00:05 +0530

    updateUi

--- components/subcomponents/cards/CuratedCard.js
+++ components/subcomponents/cards/CuratedCard.js
@@ -11,13 +11,13 @@ const CuratedCard = (props) => {
 
     return (
         <TouchableOpacity             
-            className={`flex flex-row gap-4 bg-black rounded-xl shadow-md w-[350] p-4 mt-0 mb-10 mx-0 ${isSelected ? "border-white border-2" : "border-transparent border-2"}`}
+            className={`flex flex-row gap-4 bg-black rounded-xl shadow-md w-[350] h-40 mt-0 mb-10 mx-0 ${isSelected ? "border-white border-2" : "border-transparent border-2"}`}
             onPress={() => handleShowClick({ showName: show })}
         >
             {/* Image Section */}
             <View className='h-28'>
                 <Image
-                    className="w-[150px] h-[150px] object-cover rounded-md"
+                    className="w-40 h-32 object-cover rounded-md"
                     source={{ uri: `${constants.backendUrl}/curateThumbnail/${show?.displayName}.png` }}
                     alt={show.displayName}
                 />
