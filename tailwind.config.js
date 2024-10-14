cc6af51df45c9a30e15b097fc8643a8e92a19bfc
sivaMap <sivalingam.s@map-india.org>
2024-10-14 18:19:15 +0530

    integrate geometria via tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gBold: ["Geometria-Bold", "sans-serif"],
+        gBoldItalic: ["Geometria-BoldItalic", "sans-serif"],
+        gExtraBold: ["Geometria-ExtraBold", "sans-serif"],
+        gExtraBoldItalic: ["Geometria-ExtraBoldItalic", "sans-serif"],
+        gExtraLight: ["Geometria-ExtraLight", "sans-serif"],
+        gExtraLightItalic: ["Geometria-ExtraLightItalic", "sans-serif"],
+        gHeavy: ["Geometria-Heavy", "sans-serif"],
+        gHeavyItalic: ["Geometria-HeavyItalic", "sans-serif"],
+        gItalic: ["Geometria-Italic", "sans-serif"],
+        gLight: ["Geometria-Light", "sans-serif"],
+        gLightItalic: ["Geometria-LightItalic", "sans-serif"],
+        gMediumItalic: ["Geometria-MediumItalic", "sans-serif"],
+        gThin: ["Geometria-Thin", "sans-serif"],
+        gThinItalic: ["Geometria-ThinItalic", "sans-serif"],
+        gGeometria: ["Geometria", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
