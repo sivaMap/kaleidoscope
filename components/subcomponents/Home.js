ebc3e5c05a980f3e3d13c74d6fbaba3d1760e4ce
sivaMap <sivalingam.s@map-india.org>
2024-10-15 12:44:37 +0530

    convert pressable to Touchable Opacity for buttom effect, Bug Fix=> correct the clickable area

--- components/subcomponents/Home.js
+++ components/subcomponents/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { constants } from '../../constants';
 import { useKaleidoCrud } from '../../context/kaleidoscopeCrudContext';
 
@@ -8,43 +8,43 @@ const Home = () => {
 
     return (
         <View className="flex flex-col justify-center gap-4 mt-4 mb-0 h-5/6">
-            <Pressable
-                className="flex flex-row justify-center" onPress={() => setLoadName(constants.loadScreen.curate)}>
-                <View style={styles.borderContainer}>
-                    <Text
-                        // style={{ fontFamily: fontsLoaded ? 'Geometria' : '' }}
-                        style={{
-                            backgroundColor: 'rgba(0, 0, 0, 0.2)', borderColor: 'rgba(255, 255, 255, 0.3)',
-                            // fontFamily: fontsLoaded ? 'Geometria' : '' ,"fontWeight": 700
-                        }}
-                        // style={styles.text}
-                        className={` rounded-full px-20 py-6 text-center text-base text-white ${fontsLoaded ? "font-gBold" : ""}`}
-                    >
-                        Experience a curated show
-                    </Text>
-                </View>
-            </Pressable>
+            <View className="flex flex-row justify-center ">
+                <TouchableOpacity
+                    onPress={() => setLoadName(constants.loadScreen.curate)}>
+                    <View style={styles.borderContainer}>
+                        <Text
+                            style={{
+                                backgroundColor: 'rgba(0, 0, 0, 0.2)', borderColor: 'rgba(255, 255, 255, 0.3)',
+                            }}
+                            className={` rounded-full px-20 py-6 text-center text-base text-white ${fontsLoaded ? "font-gBold" : ""}`}
+                        >
+                            Experience a curated show
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
 
             <View className="mb-3 flex flex-row justify-center items-center gap-5">
                 <View style={{ borderColor: 'rgba(255, 255, 255, 0.3)', fontFamily: fontsLoaded ? 'Geometria' : '' }} className="w-2/12 border-t mx-2" />
                 <Text
-                    // style={{ "fontWeight": 700 }}
                     className={`text-white ${fontsLoaded ? "font-gBold" : ""}`}>OR</Text>
                 <View style={{ borderColor: 'rgba(255, 255, 255, 0.3)' }} className="w-2/12 border-t mx-2" />
             </View>
 
-            <Pressable className="flex flex-row justify-center" onPress={() => setLoadName(constants.loadScreen.art)}>
-                <View style={styles.borderContainer}>
-                    <Text
-                        style={{
-                            backgroundColor: 'rgba(0, 0, 0, 0.2)', borderColor: 'rgba(255, 255, 255, 0.3)',
-                            // fontFamily: fontsLoaded ? 'Geometria' : '', "fontWeight": 700
-                        }}
-                        className={`rounded-full px-24 py-6 text-center text-base text-white ${fontsLoaded ? "font-gBold" : ""}`}>
-                        Create your own show
-                    </Text>
-                </View>
-            </Pressable>
+            <View className="flex flex-row justify-center">
+                <TouchableOpacity onPress={() => setLoadName(constants.loadScreen.art)}>
+                    <View style={styles.borderContainer}>
+                        <Text
+                            style={{
+                                backgroundColor: 'rgba(0, 0, 0, 0.2)', borderColor: 'rgba(255, 255, 255, 0.3)',
+                            }}
+                            className={`rounded-full px-24 py-6 text-center text-base text-white ${fontsLoaded ? "font-gBold" : ""}`}>
+                            Create your own show
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -52,22 +52,10 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#000', // Optional: Background color for contrast
-    },
     borderContainer: {
-        backgroundColor: 'rgba(0, 0, 0, 0.3)', // Background color for the border
-        borderColor: 'rgba(255, 255, 255, 0.3)', // Border color
-        borderWidth: 2, // Border width
-        borderRadius: 50, // Full round corners
-        // paddingHorizontal: 12, // Horizontal padding
-        // paddingVertical: 6, // Vertical padding
-    },
-    text: {
-        textAlign: 'center', // Center text
-        color: 'white', // Text color
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        borderColor: 'rgba(255, 255, 255, 0.3)',
+        borderWidth: 2,
+        borderRadius: 50,
     },
 });
