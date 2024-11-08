4c5bc64026cf25075cfd5157d82f0b426ad5f888
sivaMap <sivalingam.s@map-india.org>
2024-11-08 11:24:51 +0530

    user friendly slider upgradion, update svg, rm redundant svg

--- components/subcomponents/VolumeSlider2.js
+++ components/subcomponents/VolumeSlider2.js
@@ -7,32 +7,16 @@ import Svg, { G, Mask, Path, Rect } from 'react-native-svg';
 
 const VolumeSlider2 = ({ isShowRunning }) => {
 
-    const muteVolume = (
-        // <Svg className="-mt-2 -ml-1" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#5f6368"><Path d="M280-360v-240h160l200-200v640L440-360H280Zm66.67-66.67h122.66l104 101.34V-634l-104 100.67H346.67v106.66ZM459.33-480Z" fill="white" /></Svg>
-        <Svg className="-mt-1.5 -ml-3" xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="48px" fill="#5f6368"><Path d="M280-360v-240h160l200-200v640L440-360H280Zm60-60h127l113 109v-337L467-540H340v120Zm119-60Z" fill="white" /></Svg>
+    const muteVolume = (        
+        <Svg className="-mt-2 " xmlns="http://www.w3.org/2000/svg" height="38px" viewBox="0 -960 960 960" width="35px" fill="#5f6368"><Path d="M625.67-308.67 566.33-368l112-111.33-112-111.34L625.67-650 737-538l111.33-112 59.34 59.33-112 111.34 112 111.33-59.34 59.33-111.33-112-111.33 112ZM93-346.67v-266.66h169.33l218.67-220v706.66l-218.67-220H93ZM397-626l-98.67 96.67H177v98.66h121.33L397-333.33V-626ZM294-480.67Z" fill="white" /></Svg>
     )
 
     const lowVolume = (
-        <Svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 32 38" fill="none">
-            <Path d="M0 25.9999V11.9999H9.33333L21 0.333252V37.6666L9.33333 25.9999H0ZM25.6667 28.3333V9.54992C27.4167 10.3666 28.8264 11.6305 29.8958 13.3416C30.9653 15.0527 31.5 16.9388 31.5 18.9999C31.5 21.061 30.9653 22.9277 29.8958 24.5999C28.8264 26.2721 27.4167 27.5166 25.6667 28.3333ZM16.3333 11.6499L11.3167 16.6666H4.66667V21.3333H11.3167L16.3333 26.3499V11.6499Z" fill="white" />
-        </Svg>
-        // <Svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7" viewBox="0 0 32 38" fill="none">
-        //     <Path d="M0 25.9999V11.9999H9.33333L21 0.333252V37.6666L9.33333 25.9999H0ZM25.6667 28.3333V9.54992C27.4167 10.3666 28.8264 11.6305 29.8958 13.3416C30.9653 15.0527 31.5 16.9388 31.5 18.9999C31.5 21.061 30.9653 22.9277 29.8958 24.5999C28.8264 26.2721 27.4167 27.5166 25.6667 28.3333ZM16.3333 11.6499L11.3167 16.6666H4.66667V21.3333H11.3167L16.3333 26.3499V11.6499Z" fill="white" />
-        // </Svg>
+        <Svg className="-mt-1 -ml-1.5" xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="39px" fill="#5f6368"><Path d="M182.33-346.67v-266.66h169.34l218.66-220v706.66l-218.66-220H182.33Zm454.67 40v-346.66q56 20 88.67 67.33 32.66 47.33 32.66 106t-32.66 105.67q-32.67 47-88.67 67.66ZM486.33-626l-98.66 96.67H266.33v98.66h121.34l98.66 97.34V-626ZM374.67-480Z" fill="white" /></Svg>        
     );
 
     const highVolume = (
-        // <Svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#5f6368">
-        //     <Path d="M560-131v-68.67q94.67-27.33 154-105 59.33-77.66 59.33-176.33 0-98.67-59-176.67-59-78-154.33-104.66V-831q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm426.67 45.33v-332Q599-628 629.5-582T660-480q0 55-30.83 100.83-30.84 45.84-82.5 64.5ZM413.33-634l-104 100.67H186.67v106.66h122.66l104 101.34V-634Zm-96 154Z" />
-        // </Svg>
-        <Svg className="-mt-2.5" width="35" height="45" viewBox="0 0 45 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <Mask id="mask0_1660_26259" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="44" height="44">
-                <Rect width="44" height="44" fill="#D9D9D9" />
-            </Mask>
-            <G mask="url(#mask0_1660_26259)">
-                <Path d="M28.1286 41.9127V37.6955C31.2143 36.8041 33.7 35.0898 35.5857 32.5527C37.4714 30.0155 38.4143 27.1355 38.4143 23.9127C38.4143 20.6898 37.4714 17.8098 35.5857 15.2727C33.7 12.7355 31.2143 11.0213 28.1286 10.1298V5.91269C32.38 6.87269 35.8429 9.02412 38.5171 12.367C41.1914 15.7098 42.5286 19.5584 42.5286 23.9127C42.5286 28.267 41.1914 32.1155 38.5171 35.4584C35.8429 38.8013 32.38 40.9527 28.1286 41.9127ZM5.5 30.1355V17.7927H13.7286L24.0143 7.50698V40.4213L13.7286 30.1355H5.5ZM28.1286 32.1927V15.6327C29.74 16.387 31 17.5184 31.9086 19.027C32.8171 20.5355 33.2714 22.1813 33.2714 23.9641C33.2714 25.7127 32.8171 27.3327 31.9086 28.8241C31 30.3155 29.74 31.4384 28.1286 32.1927ZM19.9 17.4841L15.4771 21.907H9.61429V26.0213H15.4771L19.9 30.4441V17.4841Z" fill="white" />
-            </G>
-        </Svg>
+        <Svg className="-mt-2" xmlns="http://www.w3.org/2000/svg" height="38px" viewBox="0 -960 960 960" width="35px" fill="#5f6368"><Path d="M549.33-81.67v-86q108.67-27.33 177-115 68.34-87.66 68.34-199.33 0-110.67-68.5-197.67-68.5-87-176.84-114.33v-86q144.34 27.33 236.84 139.5t92.5 258.5q0 148-92.17 260.83-92.17 112.84-237.17 139.5Zm-468-265v-266.66h169.34l218.66-220v706.66l-218.66-220H81.33Zm454.67 40v-346.66q55 19.66 88.17 67.16 33.16 47.5 33.16 106.17t-33.16 106.17Q591-326.33 536-306.67ZM385.33-626l-98.66 96.67H165.33v98.66h121.34l98.66 97.34V-626ZM293-480Z" fill="white" /></Svg>        
     );
 
     const volumeChanger = 45;
@@ -76,8 +60,7 @@ const VolumeSlider2 = ({ isShowRunning }) => {
                 }}
                 className="flex flex-row items-center  w-36 pl-2 ml-2 pr-0 py-1 rounded-full">
                 <TouchableOpacity onPress={() => setSliderEnable(prev => !prev)}>
-                    <View style={{ width: 28, height: 28 }}>
-                        {/* {volume < volumeChanger ? lowVolume : highVolume} */}
+                    <View style={{ width: 28, height: 28 }}>                        
                         {volume < volumeChanger ?
                             volume === 0 ? muteVolume : lowVolume
                             : highVolume}
@@ -85,7 +68,7 @@ const VolumeSlider2 = ({ isShowRunning }) => {
                 </TouchableOpacity>
 
                 <Slider
-                    style={{ width: 110, height: 20, marginLeft: -6, marginTop: -2 }}
+                    style={{ width: 110, height: 20, marginLeft: 0, marginTop: -2 }}
                     minimumValue={0}
                     maximumValue={100}
                     step={5}
